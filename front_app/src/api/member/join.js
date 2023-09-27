38548b85b1fefb99dcb803f9f3bf21a0033da29d
yonggyo1125 <yonggyo00@kakao.com>
2023-09-27 18:43:35 +0900

    아이디 유효성 검사

--- front_app/src/api/member/join.js
+++ front_app/src/api/member/join.js
@@ -1,3 +1,13 @@
-import apiRequest from './lib/apiRequest';
+import apiRequest from '../../lib/apiRequest';
 
-export const checkUserExists = (userId) => ({});
+export const checkUserExists = (userId) => new Promise((resolve, reject) => {
+    apiRequest(`/user/exists/{userId}`)
+        .then((res) => {
+            console.log(res);
+            resolve(res);
+        })
+        .catch((err) => {
+            console.log(err);
+            reject(err);
+        });
+});
--- front_app/src/components/member/JoinForm.js
+++ front_app/src/components/member/JoinForm.js
@@ -3,7 +3,9 @@ import Title from '../common/Title';
 import StyledButton1 from '../common/StyledButton1';
 import ErrorMessage from '../common/ErrorMessage';
 import {OuterBox, InnerBox} from '../common/LayoutBox'
-import { emailValidator, mobileValidator } from '../../lib/validators';
+import { emailValidator, mobileValidator, passwordAlphaCheck, passwordNumberCheck, passwordSpecialCharCheck } from '../../lib/validators';
+import { checkUserExists } from '../../api/member/join';
+
 const initialForm = {
     email : '',
     password: '',
@@ -46,9 +48,16 @@ const JoinForm = () => {
                     throw new Error("이메일 형식이 아닙니다.");
                 }
             }
-            
+
+            const password = form.password;
+            /* 비밀번호 자리수 및 복잡성 체크 */
+            if (password.length < 8 || !passwordAlphaCheck(password) || !passwordNumberCheck(password) || !passwordSpecialCharCheck(password)) {
+                e.target.password.focus();
+                throw new Error("비밀번호는 8자 이상의 대소문자 포함 알파벳, 숫자, 특수문자로 입력하세요.")
+            }
+
             /* 비밀번호, 비밀번호 확인 처리 */
-            if (form.password !== form.confirmPassword) {
+            if (password !== form.confirmPassword) {
                 e.target.confirmPassword.focus();
                 throw new Error("비밀번호가 일치하지 않습니다.");
             }
@@ -59,11 +68,26 @@ const JoinForm = () => {
                 throw new Error("휴대전화번호 형식이 아닙니다.");
             } 
             
+           
+            (async() => {
+                try {
+                     /* 아이디 중복 여부 체크 */
+                    const res = await checkUserExists(form.email);
+                    console.log(res);
+                    
+                } catch (err) {
+                    console.log(err);
+                }
+            })();
+
             setMessage("");
        } catch (err) {
             setMessage(err.message);  
        }
        /* 유효성 검사 E */
+
+
+
     }, [form]);
 
     return (
